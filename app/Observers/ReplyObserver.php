e745fa0cccc44272cbb2d567581173acd1cdfd46
vagrant <vagrant@homestead>
2018-04-02 06:49:09 +0000

    User can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,11 +11,18 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        //HTML内容净化,防止XSS攻击
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)
     {
         //
+    }
+
+    public function created(Reply $reply)
+    {
+        //回复数量自增一
+        $reply->topic->increment('reply_count', 1);
     }
 }
