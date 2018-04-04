7ae1a423e98ecc244fb28c41c67c9705683db5d1
Crocutax <wangxw725@163.com>
2018-04-04 08:53:02 +0000

    replies destroy

--- app/Http/Requests/Api/CaptchaRequest.php
+++ app/Http/Requests/Api/CaptchaRequest.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
 
 class CaptchaRequest extends FormRequest
 {
 
-    public function authorize()
-    {
-        return true;
-    }
 
     public function rules()
     {
